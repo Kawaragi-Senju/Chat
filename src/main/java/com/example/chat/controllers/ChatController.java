fb5aec3c0090fdb54af53ef99b7542ec8071ae68
danny <dannyboykov@bk.ru>
2024-05-15 23:47:18 +0300

    Чат начало.

--- src/main/java/com/example/chat/controllers/ChatController.java
+++ src/main/java/com/example/chat/controllers/ChatController.java
@@ -1,8 +1,61 @@
 package com.example.chat.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ChatController {
 
+    @GetMapping("/main")
+    public String getMain(){
+        return "main";
+    }
+
+    @GetMapping("/login")
+    public String getLogin(){
+        return "login";
+    }
+
+    @GetMapping("/customer/{id}")
+    public String getCustomerById(){
+        return "id";
+    }
+
+    @PostMapping("/customer")
+    public String postCustomer(){
+        return "createCustomer";
+    }
+
+    @GetMapping("/dialogs/{id}")
+    public String getDialogsById(){
+        return "dialogs";
+    }
+
+    @GetMapping("/dialog/{id}")
+    public String getDialogById(){
+        return "dialog";
+    }
+
+    @DeleteMapping("/dialog/{id}")
+    public String deleteDialogById(){
+        return "delete";
+    }
+
+    @PostMapping("/dialog")
+    public String postDialog(){
+        return "dialog";
+    }
+
+    @PostMapping("/message")
+    public String postMessage(){
+        return "message";
+    }
+
+    @DeleteMapping("/message/{id}")
+    public String deleteMessage(){
+        return "";
+    }
+    // TODO: 28.04.2024
 }
