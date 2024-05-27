63a1ebb5a34fc45f7751cf5591e4e1ab6bea92b8
danny <dannyboykov@bk.ru>
2024-05-27 21:54:16 +0300

    Чат начало.

--- src/main/java/com/example/chat/security/Security.java
+++ src/main/java/com/example/chat/security/Security.java
@@ -2,6 +2,7 @@ package com.example.chat.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -10,12 +11,17 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class Security {
 
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity){
-//        httpSecurity.authorizeHttpRequests((authorizationManagerRequestMatcherRegistry) -> authorizationManagerRequestMatcherRegistry
-//                .requestMatchers("/admin").hasRole("ADMIN")
-//                .requestMatchers("Страница регистрации").permitAll()
-//                .anyRequest().denyAll();
-//
-//    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests((authorizationManagerRequestMatcherRegistry) -> authorizationManagerRequestMatcherRegistry
+                .requestMatchers("/admin").hasRole("ADMIN")
+                .requestMatchers("Страница регистрации").permitAll()
+                .anyRequest().denyAll())
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(form -> form.loginPage("Страница регистрации")
+                        .loginProcessingUrl("Процесс Логирования ")
+                        .permitAll());
+        return httpSecurity.build();
+
+    }
 }
